aeadd96151b5c4914538f39ff43ebe4a29ade03a
urey chan <urey.chan3@gmail.com>
2019-11-13 22:12:20 -0600

    wrap up catalog CRUD and features

--- angularenv/src/app/components/catalogue/catalogue.component.ts
+++ angularenv/src/app/components/catalogue/catalogue.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Catalog } from 'src/app/classes/Catalog';
 
-//TODO: Separate Post from Catalog
-import { Post } from 'src/app/classes/Post';
-
 import { CatalogueService } from 'src/app/services/catalogue.service';
 
 @Component({
@@ -14,6 +11,7 @@ import { CatalogueService } from 'src/app/services/catalogue.service';
 export class CatalogueComponent implements OnInit {
   
   catalogList: Catalog [];
+  
   //Catalog Entry Form
   catalogEntry: Catalog =
   {
@@ -33,6 +31,7 @@ export class CatalogueComponent implements OnInit {
   }
 
   //Catalog Service Methods:
+  //TODO: Refactor CRUD operations with forms. 
   
   //Get Catalog List
   getCatalog(): void {
@@ -43,8 +42,6 @@ export class CatalogueComponent implements OnInit {
   //Try getting an entry by name -->Works. But, need to find way to display. 
   //Solution: Response was in the form of an array. So, needed to display first element. 
   getCatalogEntry(catalogName: string): void {
-    //let id: number = this.catalogList.findIndex( entry => entry.Name == catalogName) + 1; //Add 1 to map to collection. 
-    //console.log(id);
     this.catalogService.getCatalogEntry(catalogName).subscribe(entry => {
       this.catalogEntry = entry;
       
@@ -60,22 +57,12 @@ export class CatalogueComponent implements OnInit {
       }
       
     });
-    //console.log(this.catalogEntry.id);
-    //console.log(this.catalogEntry.Name);
   };
 
   //Add new catalog
   addCatalog(newCatalogEntry: Catalog): void {
-    // let newcatalog: Catalog = 
-    // {
-    //   CatalogId: catalogId,
-    //   Name: catalogName, 
-    //   DateCreated: new Date().toString(), //Get the date value.     
-    //   DateModified: new Date().toString(),    
-    //   id: this.catalogList.length + 1 //Generate new ID within database
-    // };
-    
-    //Need to capture DateCreated/Modified information the moment the form is submitted.
+
+    //Filling in the rest of the form. 
     newCatalogEntry.DateCreated = new Date().toString();
     newCatalogEntry.DateModified = new Date().toString();
     newCatalogEntry.id = this.catalogList.length + 1;
